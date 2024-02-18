c403ef1cb2556e2ff9a3abfb6af853d5b231a65b
Claire Price <claire.price@mail.com>
2024-02-18 23:10:03 +0000

    add input.css and output.css files

---
+++ src/components/CardProjectDetail/CardProjectDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function CardProjectDetail({ data }) {
+    return (
+        <div>
+            <img scr={data.image} />
+            <h5>{data.title}</h5>
+            <a href={data.linkDeployed} target="_blank" rel="noreferrer noopener">Deployed application</a>
+            <a href={data.linkRepo} target="_blank" rel="noreferrer noopener">GitHub repository</a>
+        </div>
+    );
+}
+
+export default CardProjectDetail;
